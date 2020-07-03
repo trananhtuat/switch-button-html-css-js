38225c0bf6a66e1cf3adfefe9dabf0417b500a84
Tuat <akatsuki200195@gmail.com>
2020-07-03 18:01:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# switch-button-html-css-js
+ Smooth switch button use only HTML, CSS, Javascript
---
+++ index.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById('btn')
+
+function leftClick() {
+	btn.style.left = '0'
+}
+
+function rightClick() {
+	btn.style.left = '110px'
+}
